9a0bf72fe7904a10819dd3b753352d699da12d63
Sachin Ahuja <sachinahj@gmail.com>
2014-08-12 16:59:20 -0500

    changed procfile

--- public/js/concat.js
+++ public/js/concat.js
@@ -1,5 +1,6 @@
-// connect to our socket server
-var socket = io.connect('http://10.10.10.10:3000/');
+// connect to our socket server or heroku
+// var socket = io.connect('http://10.10.10.10:3000/');
+var socket = io.connect(window.location.hostname);
 
 var app = app || {};
 
